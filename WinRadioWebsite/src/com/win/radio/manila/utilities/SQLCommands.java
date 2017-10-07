f47a206bdc2ae4bc5f60c9da7ec09f865ce2a84b
Claire Austria <claire.austria@yahoo.com>
2017-10-07 23:33:16 +0800

    Inserted comments into classes to be deleted

--- WinRadioWebsite/src/com/win/radio/manila/utilities/SQLCommands.java
+++ WinRadioWebsite/src/com/win/radio/manila/utilities/SQLCommands.java
@@ -1,5 +1,5 @@
 package com.win.radio.manila.utilities;
 
 public interface SQLCommands {
-
+	/*To be deleted*/
 }
--- WinRadioWebsite/src/com/win/radio/manila/utilities/SQLOperations.java
+++ WinRadioWebsite/src/com/win/radio/manila/utilities/SQLOperations.java
@@ -1,5 +1,5 @@
 package com.win.radio.manila.utilities;
 
 public class SQLOperations implements SQLCommands {
-	
+	/*To be deleted*/
 }

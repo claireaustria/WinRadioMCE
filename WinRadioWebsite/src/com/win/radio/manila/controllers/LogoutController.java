71aede7d3f6a4992eaae986cce5f6e46c1fc69ae
elias <elias@DESKTOP-044OHFK>
2017-11-01 19:31:39 +0800

    Logout

--- WinRadioWebsite/src/com/win/radio/manila/controllers/LogoutController.java
+++ WinRadioWebsite/src/com/win/radio/manila/controllers/LogoutController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/manila/logout.html")
+@WebServlet("/manila/admin/logout.html")
 public class LogoutController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
